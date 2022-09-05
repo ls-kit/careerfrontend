6fef2d8347a9a7b2422ed96cf35e24721697242b
bashar0091 <awalbashar194@gmail.com>
2022-09-05 11:37:19 +0600

    design update

--- client/src/components/Home/CourseSlider.js
+++ client/src/components/Home/CourseSlider.js
@@ -1,36 +1,63 @@
 import React from 'react';
 import Slider from "react-slick";
-import CourseBox from '../shared/CourseBox'
+import VisibilitySensor from "react-visibility-sensor";
+import { BsFillArrowRightCircleFill, BsFillArrowLeftCircleFill } from "react-icons/bs";
 
-const CourseSlider = () => {
+function CourseSlider() {
+    const olympiad = [
+      {
+        Name: "LEADERSHIP SKILL DEVELOPMENT",
+      },
+      {
+        Name: "MANAGEMENT SKILL DEVELOPMENT",
+      },
+      {
+        Name: "MANAGEMENT SKILL DEVELOPMENT",
+      },
+      {
+        Name: "COMMUNICATION SKILL DEVELOPMENT",
+      },
+    ];
+
+    const hello = { color: "white", fontSize: "1.5em" }
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
+        nextArrow: <BsFillArrowRightCircleFill/>,
+        prevArrow: <BsFillArrowLeftCircleFill/>,
     };
+    
     return (
-        <div className="my-20 container mx-auto px-3">
-            <div>
-                <h1 class="text-4xl font-bold flex-auto my-5 text-red-700 text-center  ">Courses</h1>
-            </div>
-            <Slider {...settings}>
-            <div>
-                <CourseBox />
-            </div>
-            <div>
-                <CourseBox />
-            </div>
-            <div>
-                <CourseBox />
-            </div>
-            <div>
-                <CourseBox />
-            </div>
-            </Slider>
+      <div className="container mx-auto px-3 py-8">
+        <div>
+            <h2 class="text-3xl font-semibold flex-auto my-5 text-red-600 text-center  ">Courses</h2>
+        </div>
+        <Slider {...settings}>
+            {olympiad &&
+            olympiad.map((olympiad) => (
+            <VisibilitySensor>
+                <OlympiadCard 
+                name={olympiad.Name}
+                />
+            </VisibilitySensor>
+            ))}
+        </Slider>
+      </div>
+    );
+}
+
+function OlympiadCard({name}) {
+  
+    return (
+        <div className="border-4 border-rose-600 text-center p-10 m-4">
+            <h2 className="font-bold text-3xl text-fuchsia-600">
+               {name}
+            </h2>
         </div>
     );
-    }
+  }
 
 export default CourseSlider;
--- client/src/components/Home/OlympiadSegment.js
+++ client/src/components/Home/OlympiadSegment.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import VisibilitySensor from "react-visibility-sensor";
 
 
