57b968a685c7e6d14cfd6ed65e6a130018349379
majharul <majharuljony163@gmail.com>
2022-11-16 16:57:08 +0600

    change logo

--- client/src/components/OurTeams/CategoryCard.js
+++ client/src/components/OurTeams/CategoryCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CategoryCard = ({ name, css }) => {
     return (
         <div className={` text-center relative m-4 border-2 ${css}`}  >
-            <div className="flex flex-col shadow-xl h-full justify-center items-center py-3 px-10 ">
+            <div className="flex flex-col shadow-xl h-full justify-center items-center py-3 px-8 xl:px-10 ">
                 <p>{name}</p>
             </div>
         </div>
